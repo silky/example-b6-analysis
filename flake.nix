e2ba921258c5daf08155ad2e106d696509a73e84
Noon van der Silk <noonsilk+-noonsilk@gmail.com>
2024-09-17 09:03:11 +0100

    Example of a ChangeSet/yaml workflow

--- flake.nix
+++ flake.nix
@@ -18,11 +18,24 @@
         jupyter
         diagonal-b6.packages."${system}".python
       ]);
+      b6 = diagonal-b6.packages.${system};
+      b6-wrapped = pkgs.writeShellScriptBin "b6" ''
+        ${b6.go}/bin/b6 \
+          -http=0.0.0.0:8001 \
+          -grpc=0.0.0.0:8002 \
+          -js=${b6.b6-js.outPath} \
+          -enable-v2-ui \
+          -static-v2=${b6.frontend.outPath} \
+          "$@"
+      '';
     in
     {
       devShells.default = pkgs.mkShell {
         packages = with pkgs; [
           pyEnv
+          b6-wrapped
+          # We'll also take a whole bunch of ingest-y things
+          diagonal-b6.packages."${system}".go
         ];
       };
     });
